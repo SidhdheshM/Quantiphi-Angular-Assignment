d35759cc5eb1416f043dbc4bc650f0812e7c80ee
Sidhdhesh Modi <sidhdhesh.modi@gmail.com>
2021-08-18 02:36:39 +0530

    Add files via upload

---
+++ my-app/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { EmployeeFormComponent } from './employee-form/employee-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmployeeFormComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
